efb16037c35c1b52184a2a19308e67dfc70f5b17
goodbuddyo <sean01234@yahoo.com>
2023-01-29 10:02:42 -0500

    first commit

---
+++ src/routers/adminRouter-copy.js
@@ -0,0 +1,40 @@
+
+/* ================================== */
+/* 
+This is a copy of the initial tutorial adminRouter.js file 
+Because this file contains a db connection url 
+The actual file used is not included in this repository 
+*/
+/* ================================== */
+
+const { greenBright } = require('chalk');
+const express = require('express');
+const debug = require('debug')('app:adminRouter');
+const { MongoClient } = require('mongodb');
+const sessions = require('../data/sessions.json');
+
+const adminRouter = express.Router();
+
+adminRouter.route('/').get((req, res) => {
+  const url =
+    'mongodb+srv://dbUser:1R7jzwoc2WuKOK4U@globomantics.o6s8j.mongodb.net?retryWrites=true&w=majority';
+  const dbName = 'globomantics';
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug('Connected to the mongo DB');
+
+      const db = client.db(dbName);
+
+      const response = await db.collection('sessions').insertMany(sessions);
+      res.json(response);
+    } catch (error) {
+      debug(error.stack);
+    }
+    client.close();
+  })();
+});
+
+module.exports = adminRouter;
