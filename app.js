4fc6165edef1f861a280c2b9e66407db6e714f00
goodbuddyo <sean01234@yahoo.com>
2023-01-29 17:29:07 -0500

    added initial passport changes

--- app.js
+++ app.js
@@ -4,19 +4,37 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 const sessionsRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
+const authRouter = require('./src/routers/authRouter');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'globomantics',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+// see https://www.npmjs.com/package/express-session
+
+require('./src/config/passport.js')(app);
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/sessions', sessionsRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
